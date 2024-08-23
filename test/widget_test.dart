522eda8f638b4e1b0fd88867c0a8956149e4c05e
Nattapon Kumyaito <nattaponk@gmail.com>
2024-08-23 16:49:09 +0700

    1st stateful

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:simple_app/main.dart';
+import 'package:simple_app/main.dart.bak';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
