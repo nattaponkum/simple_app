6e375a164c56355d257ef832c599996f0ae559c8
Nattapon Kumyaito <nattaponk@gmail.com>
2024-09-03 11:49:59 +0700

    widget template

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:simple_app/screens/latestRate.dart';
-import 'package:simple_app/screens/convert.dart';
+import 'package:simple_app/screens/myApp.dart';
+// import 'package:simple_app/screens/convert.dart';
+// import 'package:simple_app/screens/latestRate.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Exchange Currency",
     home: Scaffold(
       appBar: AppBar(
-        title: const Text("Exchange Currency"),
+        title: const Text("Container Widget"),
       ),
-      body: const LatestRate(),
+      body: const MyApp(),
     ),
   ));
 }
--- lib/screens/latestRate.dart
+++ lib/screens/latestRate.dart
@@ -10,18 +10,17 @@ class LatestRate extends StatefulWidget {
 }
 
 class _LatestRateState extends State<LatestRate> {
-
   @override
   initState() {
     super.initState();
     print("Init State");
     getRate();
   }
+
   Future<Rate> getRate() async {
-    var params = {
-      "base": "THB"
-    };
-    var uri = Uri.https("currency-converter-pro1.p.rapidapi.com", "/latest-rates", params);
+    var params = {"base": "THB"};
+    var uri = Uri.https(
+        "currency-converter-pro1.p.rapidapi.com", "/latest-rates", params);
 
     var result = await http.get(uri, headers: {
       "X-RapidAPI-Host": "currency-converter-pro1.p.rapidapi.com",
@@ -32,25 +31,34 @@ class _LatestRateState extends State<LatestRate> {
     print(rate.toString());
     return rate;
   }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: getRate(), 
-      builder: (BuildContext context, AsyncSnapshot<Rate> snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text("Error: ${snapshot.error}"),
+        future: getRate(),
+        builder: (BuildContext context, AsyncSnapshot<Rate> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("Error: ${snapshot.error}"),
+              );
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data!.result!.length,
+                  itemBuilder: (context, index) {
+                    String? key = snapshot.data?.result!.keys.elementAt(index);
+                    double? value = snapshot.data?.result![key];
+                    return ListTile(
+                      title: Text(key!),
+                      subtitle: Text(value.toString()),
+                    );
+                  });
+            }
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
           }
-          return Center(
-            child: Text("Rate: ${snapshot.data!.toString()}"),
-          );
-        } else {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      });
+        });
   }
 }
